a0336581cb1696d2c221cb63879c50abfa794676
Lisandro Gabriel <saltodecuenta2023@gmail.com>
2025-04-02 14:46:09 -0300

    p

---
+++ DataLayer/Models/ProductsXSales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class ProductsXSales
+    {
+        public int idProduct { get; set; }
+        public Product product { get; set; }
+        public int idSale { get; set; }
+        public Sale sale { get; set; }
+        public int amount { get; set; }
+        public decimal subtotal { get; set; }
+        public int discount { get; set; }
+    }
+}
