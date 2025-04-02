3108a9f8465f4c132b8f88a6cb428ea1e329bc85
Lisandro Gabriel <saltodecuenta2023@gmail.com>
2025-04-02 14:49:11 -0300

    h

--- DataLayer/Repositories/CategoryRepository.cs
+++ DataLayer/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using DataLayer.Models;
+﻿using DataLayer.Interfaces;
+using DataLayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Repositories
 {
-    class ICategory: ICategory
+    class CategoryRepository: ICategory
     {
         private readonly Db_Context context;
 
