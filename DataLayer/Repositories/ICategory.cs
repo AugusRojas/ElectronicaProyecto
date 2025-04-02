d13c2a9ec7430c979e256726649210145c062a0e
Lisandro Gabriel <saltodecuenta2023@gmail.com>
2025-04-02 14:31:05 -0300

    f

---
+++ DataLayer/Repositories/ICategory.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    class ICategory: ICategory
+    {
+        private readonly Db_Context context;
+
+        public CategoryRepository(Db_Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            context.Add(category);
+            await context.SaveChangesAsync();
+        }
+
+
+        public async Task<List<Category>> GetCategories()
+        {
+            return await context.Category.ToListAsync();
+        }
+    }
+}
