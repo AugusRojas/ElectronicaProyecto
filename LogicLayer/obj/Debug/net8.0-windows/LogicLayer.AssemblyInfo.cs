9c42ac03b9f8c984ed4a145fc8b3d44f78feed1d
Lisandro Gabriel <saltodecuenta2023@gmail.com>
2025-04-02 13:13:24 -0300

    c

--- LogicLayer/obj/Debug/net8.0-windows/LogicLayer.AssemblyInfo.cs
+++ LogicLayer/obj/Debug/net8.0-windows/LogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65e16a27bf15edfb8b6a7084d68a4bd921467086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b558ac8a1e8e3583189d9585b0d33804b76b028")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
