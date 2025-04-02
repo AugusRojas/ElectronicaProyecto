f1e65463dc00196af747aca1344696d3a5382097
Lisandro Gabriel <saltodecuenta2023@gmail.com>
2025-04-02 15:01:42 -0300

    i

--- DataLayer/Interfaces/ICategory.cs
+++ DataLayer/Interfaces/ICategory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace DataLayer.Interfaces
 {
     public interface ICategory
     {
-
+        Task<List<Category>> GetCategories();
+        Task AddCategory(Category category);
+        //Task UpdateCategory(Category category);
+        //Task DeleteCategory(int id);
     }
 }
